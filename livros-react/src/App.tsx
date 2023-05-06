e2c8c3b6e30a40a65e4772d234df9ffe62f07516
ArielFSantos <arielrari@gmail.com>
2023-05-06 17:06:25 -0300

    att

--- livros-react/src/App.tsx
+++ livros-react/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import LivroLista from './LivroLista';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
   return (
