828d4b65bf6ac712d17767f63e7dc91c375796c7
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2023-05-14 11:21:28 -0300

    Aeeeeeeeee deu certo

--- livros-react/src/LivroDados.js
+++ livros-react/src/LivroDados.js
@@ -1,24 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ControladorLivro from './controle/ControleLivros';
-import { getEditoras } from "./controle/ControleEditora";
+import { getEditoras } from './controle/ControleEditora';
+
 
 function LivroDados() {
-
-    
   const controleLivro = new ControladorLivro();
   const [titulo, setTitulo] = useState('');
   const [resumo, setResumo] = useState('');
   const [autores, setAutores] = useState('');
-  const [codEditora, setCodEditora] = useState('');
+  const [editora, setEditora] = useState('');
   const [opcoes, setOpcoes] = useState([]);
   
-
   const navigate = useNavigate();
-
-  function tratarCombo(event) {
-    setCodEditora(Number(event.target.value));
-  }
 
   function incluir(event) {
     event.preventDefault();
@@ -27,16 +21,13 @@ function LivroDados() {
       titulo,
       resumo,
       autor: autores,
-      codEditora,
-    
-  
+      editora,
     };
     controleLivro.incluir(livro);
     navigate('/');
   }
 
-
-  useState(() => {
+  useEffect(() => {
     const editoras = getEditoras();
     const opcoes = editoras.map(editora => ({
       value: editora.codEditora,
@@ -49,27 +40,27 @@ function LivroDados() {
     <main>
       <h1>Dados do Livro</h1>
       <form onSubmit={incluir}>
-        <div class="form-group">
+        <div className="form-group">
           <label htmlFor="titulo">Título:</label>
-          <input class="form-control" id="exampleFormControlTextarea1" rows="3" type="text" value={titulo} onChange={e => setTitulo(e.target.value)} />
+          <input className="form-control" id="titulo" type="text" value={titulo} onChange={e => setTitulo(e.target.value)} />
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <label htmlFor="resumo">Resumo:</label>
-          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"  value={resumo} onChange={e => setResumo(e.target.value)} />
+          <textarea className="form-control" id="resumo" rows="3" value={resumo} onChange={e => setResumo(e.target.value)} />
         </div>
-        <div class="form-group">
-          <label htmlFor="codEditora">Editora:</label>
-          <select class="form-control" id="codEditora" value={codEditora} onChange={tratarCombo}>
+        <div className="form-group">
+          <label htmlFor="editora">Editora:</label>
+          <select className="form-control" id="editora" value={editora} onChange={e => setEditora(e.target.value)}>
             {opcoes.map(opcao => (
-              <option key={opcao.value} value={opcao.value}>{opcao.text}</option>
+              <option key={opcao.value} value={opcao.text}>{opcao.text}</option>
             ))}
           </select>
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <label htmlFor="autores">Autores:</label>
-          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"  value={autores} onChange={e => setAutores(e.target.value)} />
+          <textarea className="form-control" id="autores" rows="3" value={autores} onChange={e => setAutores(e.target.value)} />
         </div>
-        <button type="submit" class="btn btn-primary ">Salvar Dados</button>
+        <button type="submit" className="btn btn-primary">Salvar Dados</button>
       </form>
     </main>
   );
